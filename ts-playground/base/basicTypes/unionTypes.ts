c9c12d8e81fe508cf7d1f2f8dd153482f89f4c51
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2022-10-24 02:47:44 +0000

    lesson: Union types - finish

--- ts-playground/base/basicTypes/unionTypes.ts
+++ ts-playground/base/basicTypes/unionTypes.ts
@@ -1,4 +1,17 @@
-function add( n1: number, n2: number ): number {
-    const result = n1 + n2;
+function combine( input1: number | string, input2: number| string ): number | string  {
+    let result;
+    if(typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    } 
+    if(typeof input1 === "string" || typeof input2 === "string") {
+        result = input1.toString() + input2.toString();
+    }
     return result;
 };
+
+
+
+const combineAges = combine(30, 26);
+console.log(combineAges);
+
+const combineNames = combine("Plamen", "Mitev");
