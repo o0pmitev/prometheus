281fc9971e655eccde8b993980871027c2ca71df
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2022-10-26 04:06:30 +0000

    lesson: OOP: part 2

--- ts-playground/base/app.ts
+++ ts-playground/base/app.ts
@@ -1,11 +1,11 @@
 class Department {
     name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
     readonly id: number;
     constructor(name: string, id: number) {
         this.name = name;
         this.id = id;
-    }   
+    }
     /** 
      * (this: Department)
      * Enforces that the object that is calling this method
@@ -17,8 +17,8 @@ class Department {
      * so we need to pass it as well
      *   const accCopy = {name: "Department Name", describe: accounting.describe}
      *   accCopy.describe()
-     */ 
-    public describe(this: Department):void {
+     */
+    public describe(this: Department): void {
         console.log(`Department ${this.name}, id: ${this.id}`);
     }
 
@@ -33,12 +33,64 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting', 1);
+
+class ITDepartment extends Department {
+    public admins: string[];
+    constructor(id: number, admins: string[]) {
+        super("IT", id);
+        this.admins = admins;
+    }
+}
+
+
+class Accounting extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+
+    private reports: string[];
+    constructor(id: number, reports: string[]) {
+        super("Accounting", id);
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    //ovverides Department setter
+    public set setEmployee(name: string) {
+        if (name.length < 3) {
+            return;
+        }
+        this.employees.push(name);
+    }
+    setReports(text: string): void {
+        this.lastReport = text;
+        this.reports.push(text);
+    }
+
+    get getReports(): string[] {
+        console.log(this.reports);
+        return this.reports;
+    }
+}
+
+const it = new ITDepartment(22, ["Plamen", "Mitev"]);
+console.log(it)
+it.describe();
+
+const accounting = new Accounting(12, []);
+accounting.setReports('Something get wrong');
 accounting.setEmployee = 'Plamen';
 accounting.setEmployee = 'Velichka';
 accounting.setEmployee = 'Stefan';
 accounting.setEmployee = 'Teodor';
-
+accounting.setEmployee = 'se'
+let reports = accounting.getReports;
+console.log(accounting.mostRecentReport);
+console.log('reports ---->', reports)
 // accounting.employees[2] = "Mitev" -> error because employees is private
 console.log(accounting);
 accounting.describe();
--- ts-playground/base/dist/app.js
+++ ts-playground/base/dist/app.js
@@ -17,11 +17,52 @@ class Department {
         return this.employees;
     }
 }
-const accounting = new Department('Accounting', 1);
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super("IT", id);
+        this.admins = admins;
+    }
+}
+class Accounting extends Department {
+    constructor(id, reports) {
+        super("Accounting", id);
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+    set setEmployee(name) {
+        if (name.length < 3) {
+            return;
+        }
+        this.employees.push(name);
+    }
+    setReports(text) {
+        this.lastReport = text;
+        this.reports.push(text);
+    }
+    get getReports() {
+        console.log(this.reports);
+        return this.reports;
+    }
+}
+const it = new ITDepartment(22, ["Plamen", "Mitev"]);
+console.log(it);
+it.describe();
+const accounting = new Accounting(12, []);
+accounting.setReports('Something get wrong');
 accounting.setEmployee = 'Plamen';
 accounting.setEmployee = 'Velichka';
 accounting.setEmployee = 'Stefan';
 accounting.setEmployee = 'Teodor';
+accounting.setEmployee = 'se';
+let reports = accounting.getReports;
+console.log(accounting.mostRecentReport);
+console.log('reports ---->', reports);
 console.log(accounting);
 accounting.describe();
 accounting.getEmployeesInformation;
