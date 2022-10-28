85efe9e8f8f820f26ab5bbab32ff5e7e5fbb089d
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2022-10-28 03:02:01 +0000

    Lesson: OOP part 3

--- ts-playground/base/app.ts
+++ ts-playground/base/app.ts
@@ -1,131 +1,38 @@
-abstract class Department {
-    static fiscalYear: number = 2020;
-    name: string;
-    protected employees: string[] = [];
-    readonly id: number;
-    constructor(name: string, id: number) {
-        this.name = name;
-        this.id = id;
-    }
-    /** 
-     * (this: Department)
-     * Enforces that the object that is calling this method
-     * to have the same structure as the class
-     * example:
-     *   const accCopy = {describe: accounting.describe}
-     *   accCopy.describe()
-     * ts will complain that the accCopy is missing the name field 
-     * so we need to pass it as well
-     *   const accCopy = {name: "Department Name", describe: accounting.describe}
-     *   accCopy.describe()
-     */
+// interface Person {
+//   name: string;
+//   age: number;
 
-    static createEmployee(name: string) {
-        return { name: name };
-    }
+//   greet(phrase: string): void;
+// }
 
-    abstract describe(this: Department): void;
+// let user1: Person;
 
-    public set setEmployee(employee: string) {
-        this.employees.push(employee);
-    }
+// user1 = {
+//   name: "Plamen",
+//   age: 32,
+//   greet(phrase: string) {
+//     console.log(`${phrase} ${this.name}`)
+//   }
+// }
 
-    public get getEmployeesInformation(): string[] {
-        console.log(this.employees.length);
-        console.log(this.employees);
-        return this.employees;
-    }
+// user1.greet('Hello');
+interface Greetable {  
+  name: string;
+  greet(phrase: string): void;
+}
+  
+class Person implements Greetable {
+  public name: string;
+  private age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}, ${this.age} years old`)
+  }
 }
 
-
-class ITDepartment extends Department {
-    public admins: string[];
-    constructor(id: number, admins: string[]) {
-        super("IT", id);
-        this.admins = admins;
-    }
-    describe(this: Department): void {
-        console.log("It Department - ID:" + this.id);
-    }
-}
-
-
-class Accounting extends Department {
-    private lastReport: string;
-    private static instance: Accounting;
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('No report found');
-    }
-
-    private reports: string[];
-    private constructor(id: number, reports: string[]) {
-        super("Accounting", id);
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-
-    static getInstance() {
-        if (Accounting.instance) {
-            return this.instance
-        }
-        this.instance = new Accounting(12, []);
-        return this.instance
-    }
-
-    describe(this: Department): void {
-        console.log("THIS IS ACCOUNTING:" + this.id)
-    }
-    //ovverides Department setter
-    public set setEmployee(name: string) {
-        if (name.length < 3) {
-            return;
-        }
-        this.employees.push(name);
-    }
-    setReports(text: string): void {
-        this.lastReport = text;
-        this.reports.push(text);
-    }
-
-    get getReports(): string[] {
-        console.log(this.reports);
-        return this.reports;
-    }
-}
-
-
-
-
-
-const employee1 = Department.createEmployee('Plamen');
-console.log("Employee 1 ====> ", employee1);
-console.log(Department.fiscalYear);
-const it = new ITDepartment(22, ["Plamen", "Mitev"]);
-console.log(it)
-it.describe();
-
-const accounting = Accounting.getInstance();
-const accounting2 = Accounting.getInstance();
-console.log('they are the same object', accounting, accounting2)
-accounting.setReports('Something get wrong');
-accounting.setEmployee = 'Plamen';
-accounting.setEmployee = 'Velichka';
-accounting.setEmployee = 'Stefan';
-accounting.setEmployee = 'Teodor';
-accounting.setEmployee = 'se'
-let reports = accounting.getReports;
-console.log(accounting.mostRecentReport);
-console.log('reports ---->', reports)
-// accounting.employees[2] = "Mitev" -> error because employees is private
-console.log(accounting);
-accounting.describe();
-accounting.getEmployeesInformation;
-
-
-
-// const accountingCopy = {name: 'Accounting-copy', describe: accounting.describe};
-// accountingCopy.describe() 
+const person1 = new Person('Plamen', 32);
+person1.greet('Hello ')
--- ts-playground/base/dist/app.js
+++ ts-playground/base/dist/app.js
@@ -1,88 +1,13 @@
 "use strict";
-class Department {
-    constructor(name, id) {
-        this.employees = [];
+class Person {
+    constructor(name, age) {
         this.name = name;
-        this.id = id;
+        this.age = age;
     }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    set setEmployee(employee) {
-        this.employees.push(employee);
-    }
-    get getEmployeesInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-        return this.employees;
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}, ${this.age} years old`);
     }
 }
-Department.fiscalYear = 2020;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super("IT", id);
-        this.admins = admins;
-    }
-    describe() {
-        console.log("It Department - ID:" + this.id);
-    }
-}
-class Accounting extends Department {
-    constructor(id, reports) {
-        super("Accounting", id);
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('No report found');
-    }
-    static getInstance() {
-        if (Accounting.instance) {
-            return this.instance;
-        }
-        this.instance = new Accounting(12, []);
-        return this.instance;
-    }
-    describe() {
-        console.log("THIS IS ACCOUNTING:" + this.id);
-    }
-    set setEmployee(name) {
-        if (name.length < 3) {
-            return;
-        }
-        this.employees.push(name);
-    }
-    setReports(text) {
-        this.lastReport = text;
-        this.reports.push(text);
-    }
-    get getReports() {
-        console.log(this.reports);
-        return this.reports;
-    }
-}
-const employee1 = Department.createEmployee('Plamen');
-console.log("Employee 1 ====> ", employee1);
-console.log(Department.fiscalYear);
-const it = new ITDepartment(22, ["Plamen", "Mitev"]);
-console.log(it);
-it.describe();
-const accounting = Accounting.getInstance();
-const accounting2 = Accounting.getInstance();
-console.log('they are the same object', accounting, accounting2);
-accounting.setReports('Something get wrong');
-accounting.setEmployee = 'Plamen';
-accounting.setEmployee = 'Velichka';
-accounting.setEmployee = 'Stefan';
-accounting.setEmployee = 'Teodor';
-accounting.setEmployee = 'se';
-let reports = accounting.getReports;
-console.log(accounting.mostRecentReport);
-console.log('reports ---->', reports);
-console.log(accounting);
-accounting.describe();
-accounting.getEmployeesInformation;
+const person1 = new Person('Plamen', 32);
+person1.greet('Hello ');
 //# sourceMappingURL=app.js.map
