cae99810b167202afb00c1fd25fdf7e3105c84e8
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2022-11-01 04:22:06 +0000

    Lesson: Generics final part

--- ts-playground/base/dist/app.js
+++ ts-playground/base/dist/app.js
@@ -16,4 +16,38 @@ function newMerge(arg1, arg2) {
 const newObject = newMerge({ name: "Plamen", age: 32 }, { name: "Velichka", age: 30 });
 console.log(newObject.arg1);
 console.log(newObject.arg2.name);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = "Got 1 element";
+    }
+    else if (element.length > 0) {
+        descriptionText = "Got " + element.length + " elements";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("Hi there!"));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+console.log(extractAndConvert({ name: "Plamnen" }, "name"));
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Plamen");
+textStorage.addItem("Mitev");
+textStorage.removeItem("Plamen");
+console.log(textStorage.getItems());
 //# sourceMappingURL=app.js.map
